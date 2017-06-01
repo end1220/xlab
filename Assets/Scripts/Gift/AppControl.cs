6c868026a85ceb75a2b9dbc78378148adb2371f9
Locke <1534921818@qq.com>
2017-06-02 00:37:40 +0800

    gift

---
+++ Assets/Scripts/Gift/AppControl.cs
@@ -0,0 +1,68 @@
+﻿
+using UnityEngine;
+
+
+public class AppControl : MonoBehaviour
+{
+	public static AppControl Instance;
+
+	float ax = 0;
+	float ay = 0;
+
+	private float rotateSpeed = 10;
+
+	private float minSpeed = 10;
+	private float maxSpeed = 100;
+
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+
+	void Update()
+	{
+		//transform.Rotate(new Vector3(0, angelY, 0));
+	}
+
+
+	void LateUpdate()
+	{
+		ax += Input.acceleration.x * 30;
+		ay += -Input.acceleration.y * 30;
+
+		if (rotateSpeed > minSpeed)
+			rotateSpeed -= 20 * Time.deltaTime;
+		else if (rotateSpeed < -minSpeed)
+			rotateSpeed += 20 * Time.deltaTime;
+		else
+			rotateSpeed = rotateSpeed > 0 ? minSpeed : -minSpeed;
+		transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
+	}
+
+
+	public void OnTouchBegan()
+	{
+
+	}
+
+	public void OnTouchEnd()
+	{
+		
+	}
+
+
+	public void OnTouchMoved(float dx, float dy)
+	{
+		rotateSpeed = -dx / Time.deltaTime;
+		if (rotateSpeed > maxSpeed)
+			rotateSpeed = maxSpeed;
+		else if (rotateSpeed < -maxSpeed)
+			rotateSpeed = -maxSpeed;
+		transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
+	}
+
+
+}
+
---
+++ Assets/Scripts/Gift/TouchProcessor.cs
@@ -0,0 +1,87 @@
+﻿
+using UnityEngine;
+using Lite;
+
+
+public class TouchProcessor : MonoBehaviour
+{
+	public static TouchProcessor Inst;
+	private bool bMoveKey = false;
+
+
+	private void Awake()
+	{
+		Inst = this;
+	}
+
+	private bool bTouchBegin = false;
+
+
+	public void LateUpdate()
+	{
+		try
+		{
+			UpdateTouchScreen();
+		}
+		catch (System.Exception e)
+		{
+			Log.Error(e.ToString());
+		}
+	}
+	
+	private void UpdateTouchScreen()
+	{
+		if (Input.touchCount == 0)
+			return;
+
+		//当前触摸在UI上 ?
+		if (UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+			return;
+
+		if (Input.GetTouch(0).phase == TouchPhase.Began)
+		{
+			OnTouchBegan();
+		}
+		else if (Input.GetTouch(0).phase == TouchPhase.Ended)
+		{
+			OnTouchEnd();
+		}
+		else if (Input.GetTouch(0).phase == TouchPhase.Canceled)
+		{
+			OnTouchCanceled();
+		}
+		else if (Input.GetTouch(0).phase == TouchPhase.Moved)
+		{
+			OnTouchMoved(Input.GetTouch(0).deltaPosition);
+		}
+	}
+
+
+	private void OnTouchBegan()
+	{
+		bTouchBegin = true;
+		AppControl.Instance.OnTouchBegan();
+	}
+
+	private void OnTouchEnd()
+	{
+		bTouchBegin = false;
+		AppControl.Instance.OnTouchEnd();
+	}
+
+	private void OnTouchCanceled()
+	{
+		bTouchBegin = false;
+		AppControl.Instance.OnTouchEnd();
+	}
+
+	private void OnTouchMoved(Vector2 touchDeltaPosition)
+	{
+		if (bTouchBegin)
+		{
+			AppControl.Instance.OnTouchMoved(touchDeltaPosition.x, touchDeltaPosition.y);
+		}
+	}
+
+}
+
